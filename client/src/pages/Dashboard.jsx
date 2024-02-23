193c6c69045d3668aed473e3751236f01e9664ee
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-02-23 22:56:03 +0530

    create pages and routes

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1 className='text-red-800'>heloo dashbaord</h1>
+        </div>
+    )
+}
