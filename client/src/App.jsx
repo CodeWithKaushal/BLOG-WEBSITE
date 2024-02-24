f711cb0396f177f3016a4b892f086b1ed8364c28
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-02-24 13:35:17 +0530

    create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import SignIn from './pages/SignIn'
 
 import Projects from './pages/Projects'
 import Header from './components/Header'
-
+import Footer from './components/Footer'
 
 
 export default function App() {
@@ -28,6 +28,7 @@ export default function App() {
 
 
       </Routes>
+      <Footer />
     </BrowserRouter>
 
 
