68a367f5dc59d72c58f7efe80d2f2c75b30a9981
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-03-01 22:39:17 +0530

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,5 +8,5 @@ const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
-
+router.delete('/delete/:userId', verifyToken, deleteUser);
 export default router;
