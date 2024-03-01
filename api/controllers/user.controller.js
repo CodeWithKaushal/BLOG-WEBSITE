b9f4ad0c84a0689709f7778086748c26d0dffe8c
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-03-01 22:33:36 +0530

    complete update user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,49 +3,76 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 
 export const test = (req, res) => {
-    res.json({ message: 'api is working' });
+    res.json({ message: 'API is working!' });
 };
-
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.userId) {
-        return next(errorHandler(401, 'Your are not allow to update the user'));
+        return next(errorHandler(403, 'You are not allowed to update this user'));
     }
     if (req.body.password) {
         if (req.body.password.length < 6) {
             return next(errorHandler(400, 'Password must be at least 6 characters'));
         }
-        req.body.password = await bcryptjs.hash(req.body.password, 10);
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
     if (req.body.username) {
         if (req.body.username.length < 7 || req.body.username.length > 20) {
-            return next(errorHandler(400, 'Username must be at least 7 and 20 characters'));
+            return next(
+                errorHandler(400, 'Username must be between 7 and 20 characters')
+            );
         }
         if (req.body.username.includes(' ')) {
-            return next(errorHandler(400, 'Username must not contain spaces'));
+            return next(errorHandler(400, 'Username cannot contain spaces'));
         }
-        if (req.body.username != req.body.username.toLowerCase()) {
+        if (req.body.username !== req.body.username.toLowerCase()) {
             return next(errorHandler(400, 'Username must be lowercase'));
         }
         if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-            return next(errorHandler(400, 'Username must contain only letters and numbers'));
+            return next(
+                errorHandler(400, 'Username can only contain letters and numbers')
+            );
         }
-        try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.userId,
+            {
                 $set: {
                     username: req.body.username,
                     email: req.body.email,
-                    password: req.body.password,
                     profilePicture: req.body.profilePicture,
-
+                    password: req.body.password,
                 },
-            }, { new: true });
-            const { password, ...rest } = updatedUser._doc;
-            res.status(200).json(rest);
-
-        } catch (error) {
-            next(error);
-
-        }
+            },
+            { new: true }
+        );
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
     }
-}
+};
+
+export const deleteUser = async (req, res, next) => {
+    if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User has been deleted');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signout = (req, res, next) => {
+    try {
+        res
+            .clearCookie('access_token')
+            .status(200)
+            .json('User has been signed out');
+    } catch (error) {
+        next(error);
+    }
+};
