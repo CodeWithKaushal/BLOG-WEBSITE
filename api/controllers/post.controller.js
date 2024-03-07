4ee79d422c3d51a4f604ff168f782e53a1473737
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-03-07 20:24:24 +0530

    complete post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,27 +1,55 @@
-import Post from "../models/post.model.js";
-import { errorHandler } from "../utils/error.js"
+// import Post from "../models/post.model.js";
+// import { errorHandler } from "../utils/error.js"
 
+
+// export const create = async (req, res, next) => {
+
+//     if (!req.user.isAdmin) {
+//         return next(errorHandler(403, 'You are not allowed to create a post'))
+//     }
+//     if (!req.body.title || !req.body.content) {
+//         return next(errorHandler(400, 'Please provide all required fields'))
+//     }
+
+//     const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
+
+//     const newPost = new Post({
+//         ...req.body, slug, userId: req.user.id,
+//     });
+
+//     try {
+//         const savedPost = await newPost.save();
+//         res.send(201).json(savedPost);
+//     } catch (error) {
+//         next(error);
+//     }
+
+// };
+
+
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-
     if (!req.user.isAdmin) {
-        return next(errorHandler(403, 'You are not allowed to create a post'))
+        return next(errorHandler(403, 'You are not allowed to create a post'));
     }
     if (!req.body.title || !req.body.content) {
-        return next(errorHandler(400, 'Please provide all required fields'))
+        return next(errorHandler(400, 'Please provide all required fields'));
     }
 
     const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
 
     const newPost = new Post({
-        ...req.body, slug, userId: req.user.id,
+        ...req.body,
+        slug,
+        userId: req.user.id,
     });
 
     try {
         const savedPost = await newPost.save();
-        res.send(201).json(savedPost);
+        res.status(201).json(savedPost);
     } catch (error) {
         next(error);
     }
-
 };
