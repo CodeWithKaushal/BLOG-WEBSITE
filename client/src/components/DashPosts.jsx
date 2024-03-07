77d93f86e4bfd52a2f891848ef0ad5dbbd6a46fd
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-03-07 22:32:57 +0530

    add show more function to post of the result of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
-import { Table } from 'flowbite-react'
+import { Button, Table } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 
 export default function DashPosts() {
-    const { currentUser } = useSelector((state) => state.user)
-    const [userPosts, setUserPosts] = useState([])
+    const { currentUser } = useSelector((state) => state.user);
+    const [userPosts, setUserPosts] = useState([]);
+    const [showMore, setShowMore] = useState(true);
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -27,6 +28,26 @@ export default function DashPosts() {
             fetchPosts();
         }
     }, [currentUser._id]);
+
+    const handleShowMore = async () => {
+
+        const startIndex = userPosts.length;
+
+
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+            const data = await res.json();
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if (data.posts.length < 9) {
+                    setShowMore(false);
+                }
+            }
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return (
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar  scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -52,6 +73,7 @@ export default function DashPosts() {
                                         <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
                                         <Table.Cell><Link to={`/post/${post.slug}`}>
                                             <img src={post.image} alt={post.title} className='w-20 h-10 object-cover bg-gray-500' />
+
                                         </Link></Table.Cell>
                                         <Table.Cell>
                                             <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}> {post.title}</Link>
@@ -74,6 +96,11 @@ export default function DashPosts() {
                         }
 
                     </Table>
+                    {
+                        showMore && (
+                            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show more</button>
+                        )
+                    }
 
 
                 </>
