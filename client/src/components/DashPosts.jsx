ce94efe7ef8e074e8b22d331668e8d4bcaa1ffdb
CodeWithKaushal <kaushaldivekar08@gmail.com>
2024-03-07 22:44:26 +0530

    .

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { useState } from 'react'
-import { Button, Table } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { Button, Table } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export default function DashPosts() {
     const { currentUser } = useSelector((state) => state.user);
@@ -30,10 +30,7 @@ export default function DashPosts() {
     }, [currentUser._id]);
 
     const handleShowMore = async () => {
-
         const startIndex = userPosts.length;
-
-
         try {
             const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
             const data = await res.json();
@@ -43,11 +40,10 @@ export default function DashPosts() {
                     setShowMore(false);
                 }
             }
-
         } catch (error) {
             console.log(error.message);
         }
-    }
+    };
 
     return (
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar  scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -61,55 +57,45 @@ export default function DashPosts() {
                             <Table.HeadCell>Category</Table.HeadCell>
                             <Table.HeadCell>Delete</Table.HeadCell>
                             <Table.HeadCell>
-
                                 <span> Edit</span>
-
                             </Table.HeadCell>
                         </Table.Head>
-                        {
-                            userPosts.map((post) => (
-                                <Table.Body className='divide-y'>
-                                    <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                                        <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
-                                        <Table.Cell><Link to={`/post/${post.slug}`}>
+                        {userPosts.map((post) => (
+                            <Table.Body className='divide-y'>
+                                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                                    <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
+                                    <Table.Cell>
+                                        <Link to={`/post/${post.slug}`}>
                                             <img src={post.image} alt={post.title} className='w-20 h-10 object-cover bg-gray-500' />
-
-                                        </Link></Table.Cell>
-                                        <Table.Cell>
-                                            <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}> {post.title}</Link>
-
-                                        </Table.Cell>
-
-
-                                        <Table.Cell>{post.category}</Table.Cell>
-                                        <Table.Cell><span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span></Table.Cell>
-                                        <Table.Cell>
-
-                                            <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
-                                                <span>Edit</span>
-                                            </Link>
-
-                                        </Table.Cell>
-                                    </Table.Row>
-                                </Table.Body>
-                            ))
-                        }
-
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>
+                                            {post.title}
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>{post.category}</Table.Cell>
+                                    <Table.Cell>
+                                        <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
+                                            <span>Edit</span>
+                                        </Link>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        ))}
                     </Table>
-                    {
-                        showMore && (
-                            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show more</button>
-                        )
-                    }
-
-
+                    {showMore && (
+                        <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                            Show more
+                        </button>
+                    )}
                 </>
             ) : (
-
                 <p>You have no posts yet!</p>
-
             )}
-
         </div>
-    )
+    );
 }
