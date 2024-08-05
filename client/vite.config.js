4a31f74eebec549d7d051353cccc012ef3f9fccc
Kaushal Divekar <101656305+CodeWithKaushal@users.noreply.github.com>
2024-08-05 16:23:43 +0530

    jnd

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: parseInt(import.meta.env.VITE_PORT, 10) || 3000, // Use VITE_PORT or default to 3000
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         secure: false,
-
       },
     },
   },
